e214f9e0c840b624a47b4a5c6131c8166fe3fa8c
thoaidc <dcthoai1023@gmail.com>
2025-08-29 23:09:07 +0700

    init

---
+++ src/test/java/com/ars/paymentservice/ArsPaymentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ars.paymentservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArsPaymentServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
